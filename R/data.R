4b432005e9625bde7b6d3562967cd0085be2bc3a
John Flournoy <uoflournoy@gmail.com>
2018-04-26 11:12:05 -0400

    add data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Measurement invariance simulation fit index data
+#'
+#' 1000 iterations per condition (loading strength by number of items per factor).
+#'
+#' @format A list of lists of matrices containing differences in fit statistics
+#'   for constrained and unconstrained models.
+"mi_simulation_fit_data"
